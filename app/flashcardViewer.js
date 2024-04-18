bc2c4fb4fe7c13a3246078dc3206d1d4efa2d33e
Willem Toews <willem.toews@lethbridgecollege.ca>
2024-04-17 19:26:20 -0600

    queue logic complete and starting to create the flashcard

--- app/flashcardViewer.js
+++ app/flashcardViewer.js
@@ -23,17 +23,17 @@ export default function App() {
   const [flashcards, setFlashcards] = useState([]);
   const [filteredFlashcards, setFilteredFlashcards] = useState([]);
   const [currentFlashcard, setCurrentFlashcard] = useState(0);
+  const [queue, setQueue] = useState([]);
   const params = useLocalSearchParams();
 
   //debug
   useEffect(() => {
     console.log(params.deckId);
   }, []);
-  //debug
-  useEffect(() => {
-    console.log(filteredFlashcards);//debug
-  }, [filteredFlashcards]);
 
+  /** 
+   * Fetching
+   */
   //fetch all the flashcards at the start
   useEffect(() => {
     getFlashcards();
@@ -47,15 +47,41 @@ export default function App() {
     });
   }
 
+  /**
+   * Filtering
+   */
   // filter the flashcards based on the deck id
   // ensure to only filter after the flashcards have been updated
   useEffect(() => {
-    console.log(flashcards);//debug
+    //console.log(flashcards);//debug
     //filter the flashcards
     let filteredFlashcards = flashcards.filter(flashcard => flashcard.deck_id == params.deckId);
     setFilteredFlashcards(filteredFlashcards);
   }, [flashcards]);
 
+  //add the filtered flashcards to the queue
+  useEffect(() => {
+    //console.log(filteredFlashcards);//debug
+    setQueue(filteredFlashcards);
+  }, [filteredFlashcards]);
+
+  /**
+   * Queue logic
+   */
+  //set the current flashcard
+  useEffect(() => {
+    //randomize the queue number based on the length of the queue
+    let random = Math.floor(Math.random() * queue.length);
+    setCurrentFlashcard(random);
+    console.log("//////////////////////////");//debug
+    console.log(queue);//debug
+    //console.log(queue[random]);//debug
+  }, [queue]);
+  //remove the flashcard from the queue
+  function removeFlashcard(index) {
+    let newQueue = queue.filter((flashcard, index) => index != currentFlashcard);
+    setQueue(newQueue);
+  }
 
   return (
     <View style={Styles.container}>
@@ -63,11 +89,31 @@ export default function App() {
 
       {/* Main Content */}
 
-      {/* {flashcards.map((flashcard) => {
-          return (
-            <Text style={DeckViewerStyles.title}>{flashcard.question}</Text>
-          )
-        })} */}
+      <Text style={DeckViewerStyles.title}>
+        {queue.length > 0 ? (
+          queue[currentFlashcard]?.question || "No questions available"
+        ) : (
+          "No questions available"
+        )}
+
+      </Text>
+      <Text style={DeckViewerStyles.title}>
+        {queue.length > 0 ? (
+          queue[currentFlashcard]?.question || "No questions available"
+        ) : (
+          "No questions available"
+        )}
+
+      </Text>
+
+      {/* button to remove flashcard */}
+      <Pressable
+        style={DeckViewerStyles.button}
+        onPress={() => removeFlashcard(currentFlashcard)}
+      >
+        <Text>Remove</Text>
+      </Pressable>
+
     </View>
   )
 }
